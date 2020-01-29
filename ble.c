66e40761679d8988a0468323ffd90ec5982833bc
JunHwaPark <a28165415@gmail.com>
2020-01-29 15:07:21 +0900

    fix header error

--- ble.c
+++ ble.c
@@ -12,23 +12,26 @@
 #define DEFAULT_BAUD_RATE 115200
 #define DEFAULT_BAUD_RATE_ID 8
 
-#define BEACON_BUF_SIZE	128
+#define BEACON_BUF_SIZE 128
 
-SoftwareSerial bleSerial(RX_PIN, TX_PIN);  // RX, TX
+SoftwareSerial bleSerial(RX_PIN, TX_PIN); // RX, TX
 
 uint8_t buf[BEACON_BUF_SIZE];
 uint8_t request[10] = {0xFE, 0xFE, 0x05, 0x11, 0x02, 0x03, 0x00, 0x00, 0xFD, 0xFD};
-uint8_t history_header[12] = {0xFE, 0xFE, 0x00, 0x00, 0x12, 0x02, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00}; //history_header[3] = size, history_header[10] = payload number
+uint8_t history_header[12] = {0xFE, 0xFE, 0x00, 0x23, 0x12, 0x02, 0x02, 0x01, 0x00, 0x00, 0x01, 0x00}; //history_header[3] = size, history_header[10] = payload number
 uint8_t history_tail[2] = {0xFD, 0xFD};
 
 uint8_t payload[24] = {0x0F, 0x00, 0x11, 0x23, 0x00, 0x11, 0x00, 0x12, 0x01, 0x14, 0x01, 0x17, 0x09, 0x31, 0x06, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x00};
 
 int idx = 0;
 
-void setup() {  
+void setup()
+{
   Serial.begin(DEFAULT_BAUD_RATE);
-  while (!Serial) {}
-  
+  while (!Serial)
+  {
+  }
+
   Serial.println();
   Serial.println("**************************************************************");
   Serial.println("*** BlueEva+Sxx Software serial port communication for BLE ***");
@@ -46,45 +49,54 @@ void setup() {
   digitalWrite(RX_PIN, LOW); //4
   digitalWrite(TX_PIN, LOW); //5
 
-
   // Baud rate change
   bleSerial.begin(DEFAULT_BAUD_RATE);
   bleSerial.write("AT%B");
   bleSerial.println(DEFAULT_BAUD_RATE_ID);
   delay(10);
   bleSerial.begin(DEFAULT_BAUD_RATE);
-  while (bleSerial.available() > 0) {    // Make empty the buffer of command response
+  while (bleSerial.available() > 0)
+  { // Make empty the buffer of command response
     bleSerial.read();
   }
   memset((void *)buf, 0, (size_t)BEACON_BUF_SIZE);
-  
+
   Serial.println("Ready to send AT commands to BLE!");
   Serial.println();
 }
 
-void loop() {
-    while (bleSerial.available() > 0) {
-      sprintf((char *)&buf[idx++], "%c", bleSerial.read());
-    }
-    if (!strncmp(buf, request, 10))
-    {
-      uint8_t history[38];
-      memset((void *)history, 0, 38);
-      sprintf((char *)&history[0], "%s", history_header);
-      sprintf((char *)&history[12], "%s", payload);
-      sprintf((char *)&history[36], "%s", history_tail);
-      Serial.write("receive history request");
-      bleSerial.write(history);
-    }
-    else
-    {
-      sprintf((char *)&buf[idx++], "%c", '\0');
-      Serial.write(buf);
-    }
-    memset((void *)buf, 0, (size_t)BEACON_BUF_SIZE);
-    idx = 0;
+void loop()
+{
+  while (bleSerial.available() > 0)
+  {
+    sprintf((char *)&buf[idx++], "%c", bleSerial.read());
+  }
+  if (!strncmp(buf, request, 10))
+  {
+    uint8_t history[38];
+    memset((void *)history, 0, 38);
+    for (size_t i = 0; i < 12; i++)
+      history[i] = history_header[i];
 
-    while (Serial.available() > 0) {
-      bleSerial.write(Serial.read());
-    }
+    for (size_t i = 0; i < 24; i++)
+      history[i + 12] = payload[i];
+
+    for (size_t i = 0; i < 2; i++)
+      history[i + 36] = history_tail[i];
+
+    Serial.write("receive history request");
+    bleSerial.write(history, 38);
+  }
+  else
+  {
+    sprintf((char *)&buf[idx++], "%c", '\0');
+    Serial.write(buf, idx);
+  }
+  memset((void *)buf, 0, (size_t)BEACON_BUF_SIZE);
+  idx = 0;
+
+  while (Serial.available() > 0)
+  {
+    bleSerial.write(Serial.read());
+  }
 }
